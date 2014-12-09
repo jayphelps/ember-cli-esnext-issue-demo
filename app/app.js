869ce7aff3736e94b49f78e1104e36415e43af79
Jay Phelps <hello@jayphelps.com>
2014-12-09 09:47:44 -0800

    wowmomcoolcow

---
+++ app/app.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+var App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
+
+// This should be valid
+// https://esnext.github.io/esnext/#async%20function%20test()%20%7B%7D
+async function test() {}
+
+test();
